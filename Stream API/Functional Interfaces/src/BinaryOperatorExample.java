50041b50a484e8d1db5167755844bd0da241eb7a
Augusto Marques <markesaf16@gmail.com>
2023-08-11 15:30:04 -0300

    Update

---
+++ Stream API/Functional Interfaces/src/BinaryOperatorExample.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.Arrays;
+
+
+public class BinaryOperatorExample {
+    public static void main(String[]args){
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+
+        BinaryOperator<Integer> somar = Integer::sum;
+
+        int resultado = numeros.stream().reduce(0,Integer::sum);
+
+        System.out.println("A soma dos numeros é: "+resultado);
+
+    }
+}
---
+++ Stream API/Functional Interfaces/src/SupplierExample.java
@@ -0,0 +1,14 @@
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class SupplierExample {
+    public static void main(String[]args){
+        Supplier<String> saudacao = () -> "Ola seja bem vindo";
+
+        List<String> listaSaudacoes = Stream.generate(() -> "Ola seja bem vindo").limit(3).toList();
+
+        listaSaudacoes.forEach(System.out::println);
+    }
+}
